13e9b9816e63d4e5454ed68722c30b367742ee7d
tsaiJay <121831288+tsaiJay@users.noreply.github.com>
2024-03-25 01:34:32 +0800

    Update cp_file.py
    update ver20240325

--- cp_file.py
+++ cp_file.py
@@ -1,29 +1,32 @@
 import os
 import shutil
+from pprint import pprint
 
+def copy_one_layer_subdir(root: str, target: str):
+    sub_dirs = sorted(os.listdir(root))
+    read_dirs = [os.path.join(root, d) for d in sub_dirs]
+    save_dirs = [os.path.join(target, d) for d in sub_dirs]
 
-def copy_one_layer_subdir_filter(root: str, target: str, drop_ext: str):
-    '''
-        filter will drop files with drop_extend
-    '''
-    sub_dirs = sorted(os.listdir(ROOT_DIR))
-    read_dirs_path = [os.path.join(ROOT_DIR, d) for d in sub_dirs]
-    save_dirs_path = [os.path.join(TARGET_DIR, d) for d in sub_dirs]
+    for i in range(len(read_dirs)):
+        os.makedirs(save_dirs[i], exist_ok=True)
 
-    for i in range(len(read_dirs_path)):
-        os.makedirs(save_dirs_path[i], exist_ok=True)
+        for f in os.listdir(read_dirs[i]):
 
-        for file in os.listdir(read_dirs_path[i]):
-            if file.endswith(drop_ext) and file.startswith('kps'):
-                source_path = os.path.join(read_dirs_path[i], file)
-                target_path = os.path.join(save_dirs_path[i], file)
+            # ==== <CONDITION START> ==== add filter condition here!
+            if not f.startswith('log_'):
+                continue
+            # ==== <CONDITION END> ====
 
-                shutil.copy(source_path, target_path)
-        print('id {} done'.format(read_dirs_path[i]))
+            source_path = os.path.join(read_dirs[i], f)
+            target_path = os.path.join(save_dirs[i], f)
+            shutil.copy(source_path, target_path)
+            # print(source_path, target_path)
+
+        print('id {} done'.format(read_dirs[i]))
 
 
 if __name__ == "__main__":
     ROOT_DIR = "./dataset_xyz_8736"
-    TARGET_DIR = "./download_8736_only_kps"
+    TARGET_DIR = "./new_dataset_xyz_8736"
 
-    copy_one_layer_subdir_filter(ROOT_DIR, TARGET_DIR, drop_ext='.json')
+    copy_one_layer_subdir(ROOT_DIR, TARGET_DIR)
